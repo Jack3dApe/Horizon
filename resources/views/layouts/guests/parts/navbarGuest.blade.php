12a452a0341ad29821d907f812271c319a52f56c
Vasile Zbi <vasilezbiemp@gmail.com>
2025-01-21 22:54:28 +0000

    navbar dropdown

--- resources/views/layouts/guests/parts/navbarGuest.blade.php
+++ resources/views/layouts/guests/parts/navbarGuest.blade.php
@@ -1,17 +1,19 @@
 
-<header class="header">
+<header class="header" style="margin-bottom: -12px">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-2">
+        <div class="row align-items-center">
+            <div class="col-6 col-lg-2">
                 <div class="header__logo">
                     <a href="{{ route('home') }}">
-                        <img src="{{ asset('imgs/logo_horizon_text2.png') }}" alt="Logo" class="img-fluid w-85">
+                        <img src="{{ asset('imgs/logo_horizon_text2.png') }}" alt="Logo" class="img-fluid w-60" >
                     </a>
                 </div>
             </div>
-            <div class="col-lg-8">
+
+            <!-- Desktop Navigation -->
+            <div class="col-lg-8 d-none d-lg-block">
                 <div class="header__nav">
-                    <nav class="header__menu mobile-menu">
+                    <nav class="header__menu">
                         <ul>
                             <li class="active"><a href="{{ route('home') }}">{{__('messages.homepage')}}</a></li>
                             <li>
@@ -24,32 +26,45 @@
                             </li>
                             <li><a href="#">{{__('messages.aboutus')}}</a></li>
                             <li><a href="#">{{__('messages.support')}}</a></li>
-
                         </ul>
                     </nav>
                 </div>
             </div>
-            <div class="col-lg-2">
+
+            <div class="col-6 col-lg-2 d-lg-none text-end">
+                <button id="burger-menu-toggle" class="burger-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+
+
+            <div class="col-lg-2 d-flex align-items-center justify-content-end">
                 <div class="header__right d-flex align-items-center justify-content-end">
                     <a href="#" class="search-switch"><span class="icon_search"></span></a>
-                    <a href="{{ route('login') }}"><span class="icon_profile"></span></a>
+                    <div class="dropdown-menu dropdown-menu-demo">
+                        <a href="{{ route('login') }}"><span class="icon_profile"></span>
+                            @auth
+                                @if(auth()->user()->hasRole('admin'))
+                                    <a href="{{ route('admin.dashboard') }}"><span class="icon_desktop"></span></a>
+                                @endif
+                            @endauth
+                            @auth
+                                @if(auth()->user()->hasRole('clients'))
+                                    <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                                        @csrf
+                                        <button type="submit" style="background: none; border: none; color: white;">
+                                            <span class="icon_error-triangle_alt"></span>
+                                        </button>
+                                    </form>
+                                @endif
+                            @endauth
+                        </a>
 
-                    @auth
-                        @if(auth()->user()->hasRole('admin'))
-                            <a href="{{ route('admin.dashboard') }}"><span class="icon_desktop"></span></a>
-                        @endif
-                    @endauth
-                    @auth
-                        @if(auth()->user()->hasRole('clients'))
-                            <form action="{{ route('logout') }}" method="POST" style="display: inline;">
-                                @csrf
-                                <button type="submit" style="background: none; border: none; color: white;">
-                                    <span class="icon_error-triangle_alt"></span>
-                                </button>
-                            </form>
-                        @endif
-                    @endauth
-                    <x-language-switch />
+
+                        <x-language-switch />
+                    </div>
                 </div>
             </div>
         </div>
