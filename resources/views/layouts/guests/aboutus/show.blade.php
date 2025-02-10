bc05629893bffcc565b0179ba9d537c0af1bf60a
Alexandre Rocha <alexandre26614@hotmail.com>
2025-02-10 17:54:43 +0000

    FAQ

--- resources/views/layouts/guests/aboutus/show.blade.php
+++ resources/views/layouts/guests/aboutus/show.blade.php
@@ -5,57 +5,130 @@
 
         <div class="row justify-content-center">
             <!-- FAQ (Perguntas Frequentes) -->
-            <div class="col-12 col-lg-6 mb-5">
+            <div class="col-12 col-lg-8 mb-5">
                 <div class="card w-100 shadow-sm" style="background-color: #070720; border: none;">
                     <div class="card-header" style="background-color: #e53637; border-bottom: 2px solid #ffffff;">
                         <h3 class="card-title text-white"><i class="ti ti-book"></i> {{__('messages.freqquestions')}}</h3>
                     </div>
                     <div class="card-body" style="color: #ffffff;">
-                        <div class="accordion" id="faqAccordion">
-                            <div class="accordion-item" style="background-color: #070720; border: none;">
-                                <h2 class="accordion-header">
-                                    <button class="accordion-button" data-bs-toggle="collapse" data-bs-target="#faq1" style="background-color: #e53637; color: #ffffff;">
-                                        <i class="ti ti-info-circle me-2"></i> {{__('messages.welcomeserv')}}
-                                    </button>
-                                </h2>
-                                <div id="faq1" class="accordion-collapse collapse show" data-bs-parent="#faqAccordion">
-                                    <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
-                                        <p style="color: white">{{__('messages.welcomeservmsg')}}</p>
+                        <div class="row">
+                            <!-- Coluna da esquerda -->
+                            <div class="col-md-6">
+                                <div class="accordion" id="faqAccordionLeft">
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button" data-bs-toggle="collapse" data-bs-target="#faq1Left" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-info-circle me-2"></i> {{__('messages.welcomeserv')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq1Left" class="accordion-collapse collapse show" data-bs-parent="#faqAccordionLeft">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.welcomeservmsg')}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq2Left" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-users me-2"></i> {{__('messages.whoweare')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq2Left" class="accordion-collapse collapse" data-bs-parent="#faqAccordionLeft">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.whowearemsg')}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq3Left" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-map-pin me-2"></i> {{__('messages.wherearewe')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq3Left" class="accordion-collapse collapse" data-bs-parent="#faqAccordionLeft">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.wherearewemsg')}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq4Left" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-heart me-2"></i> {{__('messages.ourvalues')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq4Left" class="accordion-collapse collapse" data-bs-parent="#faqAccordionLeft">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.ourvaluesmsg')}}</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="accordion-item" style="background-color: #070720; border: none;">
-                                <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq2" style="background-color: #e53637; color: #ffffff;">
-                                        <i class="ti ti-users me-2"></i> {{__('messages.whoweare')}}
-                                    </button>
-                                </h2>
-                                <div id="faq2" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
-                                    <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
-                                        <p style="color: white">{{__('messages.whowearemsg')}}</p>
+                            <!-- Coluna da direita -->
+                            <div class="col-md-6">
+                                <div class="accordion" id="faqAccordionRight">
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button" data-bs-toggle="collapse" data-bs-target="#faq1Right" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-credit-card me-2"></i> {{__('messages.paymentmethodscreditpaypal')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq1Right" class="accordion-collapse collapse show" data-bs-parent="#faqAccordionRight">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.paymentmethodscreditpaypalmsg')}}</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
 
-                            <div class="accordion-item" style="background-color: #070720; border: none;">
-                                <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq3" style="background-color: #e53637; color: #ffffff;">
-                                        <i class="ti ti-heart me-2"></i> {{__('messages.ourvalues')}}
-                                    </button>
-                                </h2>
-                                <div id="faq3" class="accordion-collapse collapse" data-bs-parent="#faqAccordion">
-                                    <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
-                                        <p style="color: white">{{__('messages.ourvaluesmsg')}}</p>
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq2Right" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-mail me-2"></i> {{__('messages.whatemailactivatedo')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq2Right" class="accordion-collapse collapse" data-bs-parent="#faqAccordionRight">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.whatemailactivatedomsg')}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq3Right" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-medal me-2"></i> {{__('messages.howandwhatmedal')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq3Right" class="accordion-collapse collapse" data-bs-parent="#faqAccordionRight">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.howandwhatmedalmsg')}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion-item" style="background-color: #070720; border: none;">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#faq4Right" style="background-color: #e53637; color: #ffffff;">
+                                                <i class="ti ti-language me-2"></i> {{__('messages.currentlanguagespten')}}
+                                            </button>
+                                        </h2>
+                                        <div id="faq4Right" class="accordion-collapse collapse" data-bs-parent="#faqAccordionRight">
+                                            <div class="accordion-body" style="background-color: #070720; color: #ffffff;">
+                                                <p style="color: white">{{__('messages.currentlanguagesptenmsg')}}</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> <!-- Fim row -->
                     </div>
                 </div>
             </div>
-
-
         </div>
     </div>
 @endsection
